0071f8a89d77bf6b2f8f8540fae378c64e099906
Burak Dalyanda <burak.dalyanda@hotmail.com>
2023-01-18 21:25:22 +0300

    Removed unnecessary methods data

--- admin/App/Models/Application.php
+++ admin/App/Models/Application.php
@@ -25,18 +25,6 @@ class Application
         return DB::table('applications')->orderBy('ID', 'desc')->limit($limit)->getAll();
     }
 
-    public function getTodayApplications(int $limit = 5)
-    {
-        $date_first = Date::now()->get('Y-m-d 00:00:00');
-        $date_second = Date::now()->get('Y-m-d 23:59:59');
-
-        return DB::table('applications')
-            ->where('appointment_date', '>=', $date_first)
-            ->where('appointment_date', '<=', $date_second)
-            ->orderBy('ID', 'desc')
-            ->limit($limit)->getAll();
-    }
-
     public function getApplicationCount()
     {
         return DB::table('applications')->select('COUNT(*) AS count')->getRow();
