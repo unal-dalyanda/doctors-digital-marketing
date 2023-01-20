5d85544cf57e48c856dc893e7b3e4927adf31c2a
Burak Dalyanda <burak.dalyanda@hotmail.com>
2023-01-20 23:49:49 +0300

    Added methods for category system to Blog Model.

--- admin/App/Models/Blog.php
+++ admin/App/Models/Blog.php
@@ -9,14 +9,31 @@ class Blog
 
     public function getBlogsAdmin()
     {
-        return DB::table('blogs')->select('ID, title, status, publish_date')->orderBy('ID', 'desc')->getAll();
+        return DB::table('blogs as blogs')
+            ->select('blogs.ID, blogs.title, blogs.status, blogs.publish_date, blogs.category_id, cat.category_name')
+            ->leftJoin('categories as cat', 'cat.ID=blogs.category_id')
+            ->getAll();
     }
 
-    public function getBlogCount()
+    public function getBlogCount(int $categoryId = null)
     {
-        return DB::table('blogs')->select('COUNT(*) AS count')->where('status', '=', 1)->getRow();
+        if (!is_null($categoryId)) {
+            return DB::table('blogs')
+                ->select('COUNT(*) AS count')
+                ->where('status', '=', 1)
+                ->where('category_id', '=', $categoryId)
+                ->getRow();
+        } else {
+            return DB::table('blogs')
+                ->select('COUNT(*) AS count')
+                ->where('status', '=', 1)
+                ->getRow();
+        }
     }
 
+    /**
+     * @return mixed
+     */
     public function getStickyBlogs()
     {
         return DB::table('blogs')
@@ -27,14 +44,37 @@ class Blog
             ->getAll();
     }
 
+    /**
+     * @param int $initial
+     * @param int $limit
+     * @return mixed
+     */
     public function getBlogs(int $initial, int $limit)
     {
-        return DB::table('blogs')
-            ->select('ID, title, slug, cover_image, seo_description, status, publish_date')
-            ->where('status', '=', 1)
-            ->where('type', '!=', 0)
+        return DB::table('blogs as blogs')
+            ->select('blogs.ID, blogs.title, blogs.slug, blogs.status, blogs.cover_image, blogs.seo_description, blogs.publish_date, blogs.category_id, cat.category_name')
+            ->leftJoin('categories as cat', 'cat.ID=blogs.category_id')
+            ->where('blogs.status', '=', 1)
             ->limit($initial, $limit)
-            ->orderBy('publish_date', 'asc')
+            ->orderBy('blogs.publish_date', 'asc')
+            ->getAll();
+    }
+
+    /**
+     * @param int $initial
+     * @param int $limit
+     * @param int $categoryId
+     * @return mixed
+     */
+    public function getBlogsWithCategoryId(int $initial, int $limit, int $categoryId)
+    {
+        return DB::table('blogs as blogs')
+            ->select('blogs.ID, blogs.title, blogs.slug, blogs.status, blogs.cover_image, blogs.seo_description, blogs.publish_date, blogs.category_id, cat.category_name')
+            ->leftJoin('categories as cat', 'cat.ID=blogs.category_id')
+            ->where('blogs.status', '=', 1)
+            ->where('blogs.category_id', '=', $categoryId)
+            ->limit($initial, $limit)
+            ->orderBy('blogs.publish_date', 'asc')
             ->getAll();
     }
 
@@ -48,8 +88,14 @@ class Blog
         return DB::table('blogs')->where('slug', '=', $blogSlug)->getRow();
     }
 
-    public function getCoverImage(int $blogId){
+    public function getCoverImage(int $blogId)
+    {
         return DB::table('blogs')->select('cover_image')->where('ID', '=', $blogId)->getRow();
+    }
+
+    public function getCategories()
+    {
+        return DB::table('categories')->getAll();
     }
 
     public function addBlog($data): int
@@ -65,5 +111,15 @@ class Blog
     public function deleteBlog(int $blogId): int
     {
         return DB::table('blogs')->where('ID', '=', $blogId)->delete();
+    }
+
+    public function addCategory($data): int
+    {
+        return DB::table('categories')->insert($data);
+    }
+
+    public function deleteCategory(int $categoryId): int
+    {
+        return DB::table('categories')->where('ID', '=', $categoryId)->delete();
     }
 }
